fc062044f3f64a614296c844a257050f809d8fb7
Ryan Marcotte <ryanm@iqmetrix.com>
2012-11-21 10:46:38 -0800

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,13 @@
-﻿namespace app
+﻿using System;
+namespace app
 {
   public class Calculator
   {
     public int add(int i, int i1)
     {
+		if ((i > 0) && (i1 < 0)) { throw (new ArgumentException("i1")); }
+		else if ((i < 0) && (i1 > 0)) { throw (new ArgumentException("i")); }
+
         return i + i1;
     }
   }
